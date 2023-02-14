ba13bf5ee2fdc9bd4a3bc62d37b6da9cd5c87dd8
Grzegorz Krawczyk <g.r.krawczyk@wp.pl>
2023-02-14 23:03:13 +0100

    podzielono klase KsiazkaAdresowa na mniejsze klasy

---
+++ UzytkownikMenadzer.cpp
@@ -0,0 +1,60 @@
+#include "UzytkownikMenadzer.h"
+
+int UzytkownikMenadzer::pobierzIdNowegoUzytkownika() {
+    if (uzytkownicy.empty() == true)
+        return 1;
+    else
+        return uzytkownicy.back().pobierzId() + 1;
+}
+
+bool UzytkownikMenadzer::czyIstniejeLogin(string login) {
+    for (int i = 0; i < uzytkownicy.size(); i++) {
+        if (uzytkownicy[i].pobierzLogin() == login) {
+            cout << endl << "Istnieje uzytkownik o takim loginie." << endl;
+            return true;
+        } else {
+            return false;
+        };
+    };
+}
+
+void UzytkownikMenadzer::rejestracjaUzytkownika() {
+    Uzytkownik uzytkownik = podajDaneNowegoUzytkownika();
+
+    uzytkownicy.push_back(uzytkownik);
+    plikZUzytkownikami.dopiszUzytkownikaDoPliku(uzytkownik);
+
+    cout << endl << "Konto zalozono pomyslnie" << endl << endl;
+    system("pause");
+}
+
+void UzytkownikMenadzer::wypiszWszystkichUzytkownikow()
+{
+    for (int i = 0; i < uzytkownicy.size(); i++)
+    {
+        cout << uzytkownicy[i].pobierzId() << endl;
+        cout << uzytkownicy[i].pobierzLogin() << endl;
+        cout << uzytkownicy[i].pobierzHaslo() << endl;
+    }
+}
+
+Uzytkownik UzytkownikMenadzer::podajDaneNowegoUzytkownika() {
+    Uzytkownik uzytkownik;
+
+    uzytkownik.ustawId(pobierzIdNowegoUzytkownika());
+
+    string login;
+    string haslo;
+
+    do {
+        cout << "Podaj login: ";
+        cin >> login;
+        uzytkownik.ustawLogin(login);
+    } while (czyIstniejeLogin(login) == true);
+
+    cout << "Podaj haslo: ";
+    cin >> haslo;
+    uzytkownik.ustawaHaslo(haslo);
+
+    return uzytkownik;
+}
